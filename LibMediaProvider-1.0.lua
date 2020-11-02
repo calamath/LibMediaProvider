266cbc8ab1632fd414197ede9fffd8f1310d6ea2
ArtOfShred <artofshred@artofshred.net>
2020-11-01 20:25:21 -0500

    r16

--- LibMediaProvider-1.0.lua
+++ LibMediaProvider-1.0.lua
@@ -1,8 +1,8 @@
 --LibMediaProvider-1.0 is inspired by and borrows from LibSharedMedia-3.0 for World of Warcraft by Elkano
 --LibSharedMedia-3.0 and LibMediaProvider-1.0 are under the LGPL-2.1 license
- 
+
 --Register LMP with LibStub
-local MAJOR, MINOR = "LibMediaProvider-1.0", 13
+local MAJOR, MINOR = "LibMediaProvider-1.0", 16
 local LMP, oldminor
 if LibStub then
     LMP, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
